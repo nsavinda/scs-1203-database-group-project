1468db2486dc2d73c49fbb480c243516d66f343e
nsavinda <nirmalsavinda29@gmail.com>
2022-09-21 12:59:54 +0530

    first

--- suwasahana.sql
+++ suwasahana.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 172.17.0.1
--- Generation Time: Sep 18, 2022 at 12:42 AM
+-- Generation Time: Sep 21, 2022 at 05:59 AM
 -- Server version: 10.9.2-MariaDB
 -- PHP Version: 8.0.23
 
@@ -35,6 +35,17 @@ CREATE TABLE `attentent` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bed`
+--
+
+CREATE TABLE `bed` (
+  `bed_id` int(4) NOT NULL,
+  `ward_no` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `cleaner`
 --
 
@@ -48,6 +59,34 @@ CREATE TABLE `cleaner` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `diagnose`
+--
+
+CREATE TABLE `diagnose` (
+  `diagnose_Code` int(5) NOT NULL,
+  `emp_no` int(10) NOT NULL,
+  `patient_id` int(10) NOT NULL,
+  `diagnosis_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `diagnosis_date` date NOT NULL,
+  `diagnosis_time` time NOT NULL,
+  `description` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diagnostic_unit`
+--
+
+CREATE TABLE `diagnostic_unit` (
+  `diagnostic_unit_no` int(3) NOT NULL,
+  `diagnostic_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unit_id` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `doctor`
 --
 
@@ -56,6 +95,15 @@ CREATE TABLE `doctor` (
   `emp_id` int(10) NOT NULL,
   `dea_no` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`doctor_id`, `emp_id`, `dea_no`) VALUES
+(11, 19, 4352),
+(12, 20, 5222),
+(13, 21, 24362);
 
 -- --------------------------------------------------------
 
@@ -71,6 +119,35 @@ CREATE TABLE `doctor_special` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `drug`
+--
+
+CREATE TABLE `drug` (
+  `drug_code` int(5) NOT NULL,
+  `drug_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unit_cost` decimal(5,0) NOT NULL,
+  `drug_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `treatment_code` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergerncy`
+--
+
+CREATE TABLE `emergerncy` (
+  `patient_id` int(10) NOT NULL,
+  `first_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `relationship` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_number` char(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `employee`
 --
 
@@ -79,7 +156,37 @@ CREATE TABLE `employee` (
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `contact_no` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+  `status` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`emp_id`, `name`, `address`, `contact_no`, `status`) VALUES
+(1, 'new', 'cvcv fvf', '54565773', NULL),
+(2, 'octor', 'dfdff', '4545634', NULL),
+(8, 'new one', 'abd Rd,dsd', '45945324', NULL),
+(12, 'abc', 'abc', '123', NULL),
+(15, 'Sanduni Withanage', 'colombo 03', '075323265', NULL),
+(18, 'Nimesha Senanayake', 'Anuradhapura', '0716539324', NULL),
+(19, 'kamal herath', 'Neluwa rd', '071654883', NULL),
+(20, 'Lakmal withana', 'Hambanthota', '071637332', NULL),
+(21, 'wishwa Senanayake', 'colombo 08', '078326272', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `insurance_company`
+--
+
+CREATE TABLE `insurance_company` (
+  `patient_id` int(10) NOT NULL,
+  `company_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `branch_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `branch_address` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_address` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_no` char(10) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -91,9 +198,21 @@ CREATE TABLE `employee` (
 CREATE TABLE `medical_staff` (
   `emp_id` int(10) NOT NULL,
   `joined_date` date NOT NULL,
-  `resign_date` date NOT NULL,
+  `resign_date` date DEFAULT NULL,
   `council_regno` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `medical_staff`
+--
+
+INSERT INTO `medical_staff` (`emp_id`, `joined_date`, `resign_date`, `council_regno`) VALUES
+(12, '2020-01-19', '2020-01-22', 123),
+(15, '2022-09-14', '2022-09-06', 23265),
+(18, '2021-02-07', '2022-09-13', 43763),
+(19, '2022-09-06', '2022-09-16', 5422),
+(20, '2022-09-04', '2022-09-06', 5263),
+(21, '2022-09-05', '2022-09-23', 9087);
 
 -- --------------------------------------------------------
 
@@ -117,6 +236,102 @@ CREATE TABLE `nurse` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `nurse`
+--
+
+INSERT INTO `nurse` (`nurse_id`, `emp_id`) VALUES
+(3, 15),
+(5, 18);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patient_id` int(10) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `arrival_date` date DEFAULT NULL,
+  `arrival_time` time DEFAULT NULL,
+  `admitted_date` date DEFAULT NULL,
+  `admitted_time` time DEFAULT NULL,
+  `discharged_date` date DEFAULT NULL,
+  `discharged_time` time DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `blood_pressure` float DEFAULT NULL,
+  `temperature` float DEFAULT NULL,
+  `pulse` int(11) DEFAULT NULL,
+  `vital_date` date DEFAULT NULL,
+  `vital_time` time DEFAULT NULL,
+  `in_flag` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `out_flag` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `admitted_doctor` int(10) DEFAULT NULL,
+  `insurance_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ward_no` int(3) DEFAULT NULL,
+  `bed_id` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`patient_id`, `name`, `arrival_date`, `arrival_time`, `admitted_date`, `admitted_time`, `discharged_date`, `discharged_time`, `weight`, `blood_pressure`, `temperature`, `pulse`, `vital_date`, `vital_time`, `in_flag`, `out_flag`, `admitted_doctor`, `insurance_name`, `ward_no`, `bed_id`) VALUES
+(1, 'Nimal Galagedara', NULL, NULL, '2021-05-13', '12:50:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4, 6),
+(3, 'R. Premalal', NULL, NULL, '2021-01-07', '09:56:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 12, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_care_unit`
+--
+
+CREATE TABLE `patient_care_unit` (
+  `unit_id` int(5) NOT NULL,
+  `emp_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `symptoms`
+--
+
+CREATE TABLE `symptoms` (
+  `patient_id` int(10) NOT NULL,
+  `emp_no` int(10) NOT NULL,
+  `record_time` time NOT NULL,
+  `record_date` date NOT NULL,
+  `sysmptom` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `treatment`
+--
+
+CREATE TABLE `treatment` (
+  `emp_id` int(10) NOT NULL,
+  `treatment_id` int(5) NOT NULL,
+  `patient_id` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ward`
+--
+
+CREATE TABLE `ward` (
+  `ward_no` int(3) NOT NULL,
+  `ward_name` int(50) NOT NULL,
+  `unit_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
 -- Indexes for dumped tables
 --
 
@@ -127,10 +342,28 @@ ALTER TABLE `attentent`
   ADD PRIMARY KEY (`emp_id`);
 
 --
+-- Indexes for table `bed`
+--
+ALTER TABLE `bed`
+  ADD PRIMARY KEY (`bed_id`);
+
+--
 -- Indexes for table `cleaner`
 --
 ALTER TABLE `cleaner`
   ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `diagnose`
+--
+ALTER TABLE `diagnose`
+  ADD PRIMARY KEY (`diagnose_Code`);
+
+--
+-- Indexes for table `diagnostic_unit`
+--
+ALTER TABLE `diagnostic_unit`
+  ADD PRIMARY KEY (`diagnostic_unit_no`);
 
 --
 -- Indexes for table `doctor`
@@ -144,6 +377,12 @@ ALTER TABLE `doctor`
 --
 ALTER TABLE `doctor_special`
   ADD PRIMARY KEY (`doctor_id`);
+
+--
+-- Indexes for table `drug`
+--
+ALTER TABLE `drug`
+  ADD PRIMARY KEY (`drug_code`);
 
 --
 -- Indexes for table `employee`
@@ -171,14 +410,82 @@ ALTER TABLE `nurse`
   ADD UNIQUE KEY `emp_id` (`emp_id`);
 
 --
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patient_id`);
+
+--
+-- Indexes for table `patient_care_unit`
+--
+ALTER TABLE `patient_care_unit`
+  ADD PRIMARY KEY (`unit_id`,`emp_id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `ward`
+--
+ALTER TABLE `ward`
+  ADD PRIMARY KEY (`ward_no`),
+  ADD UNIQUE KEY `unit_id` (`unit_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bed`
+--
+ALTER TABLE `bed`
+  MODIFY `bed_id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `diagnose`
+--
+ALTER TABLE `diagnose`
+  MODIFY `diagnose_Code` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `diagnostic_unit`
+--
+ALTER TABLE `diagnostic_unit`
+  MODIFY `diagnostic_unit_no` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `doctor_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `drug`
+--
+ALTER TABLE `drug`
+  MODIFY `drug_code` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `emp_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `emp_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `nurse`
+--
+ALTER TABLE `nurse`
+  MODIFY `nurse_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `patient_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `ward`
+--
+ALTER TABLE `ward`
+  MODIFY `ward_no` int(3) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -225,6 +532,18 @@ ALTER TABLE `non_medical_staff`
 --
 ALTER TABLE `nurse`
   ADD CONSTRAINT `nurse_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `patient_care_unit`
+--
+ALTER TABLE `patient_care_unit`
+  ADD CONSTRAINT `patient_care_unit_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ward`
+--
+ALTER TABLE `ward`
+  ADD CONSTRAINT `ward_ibfk_1` FOREIGN KEY (`unit_id`) REFERENCES `patient_care_unit` (`unit_id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
